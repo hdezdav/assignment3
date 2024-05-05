2354ee64f5c8610405a0d1252a6cf91c10f8e189
hdezdav <157717974+hdezdav@users.noreply.github.com>
2024-05-05 14:04:52 -0500

    nombre

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        nombre();
+    }
+
+    public static void nombre() {
+        System.out.println("Assignment 3 by David Hernández");
+    }
+}
